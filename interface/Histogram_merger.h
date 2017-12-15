dd31a880031d191627e7c8e64d98d31e85b4c4b1
Adam Hunyadi <hunyadix@gmail.com>
2017-12-15 14:26:11 +0100

    Add option to save histogram as images

--- interface/Histogram_merger.h
+++ interface/Histogram_merger.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <stack>
 #include <string>
 #include <algorithm>
 #include <iostream>
@@ -8,12 +9,16 @@
 
 #include "TFile.h"
 #include "TH1.h"
+#include "TCanvas.h"
 
 struct Histogram_merger_options
 {
 	Histogram_merger_options(const Histogram_merger_options&) = delete; // Move only (or copy assign explicitly)
 	std::vector<std::string> input_file_path_list;
 	std::string output_file_path;
+	bool save_histograms_as_images;
+	std::string histogram_save_directory;
+	std::string histogram_image_format;
 };
 
 class Histogram_merger_program
@@ -23,13 +28,18 @@ class Histogram_merger_program
 		Histogram_merger_program(Histogram_merger_options&& t_options);
 		~Histogram_merger_program() {};
 		void fetch_histograms_from_input_files();
-		void handle_data_object(const std::vector<std::string>& t_path, TObject* t_object);
-		void copy_histogram_to_output_file(const std::vector<std::string>& t_path, TH1* histogram);
+		void handle_data_object(const std::vector<std::string>& t_path, const TObject* t_object);
+		void copy_histogram_to_output_file(const std::vector<std::string>& t_path, const TH1* histogram);
+		void save_all_histograms_as_images();
 		static std::vector<std::string> append_to_path_copy(const std::vector<std::string>& t_path, const std::string& t_directory);
+	private:
+		[[noreturn, gnu::cold]] static void e_file_missing() { throw std::runtime_error("Input file is missing."); }
 	private:
 		std::vector<std::string> m_input_file_path_list;
 		std::string m_output_file_path;
 		std::vector<TFile*> m_input_files;
 		TFile* m_output_file;
-		[[noreturn, gnu::cold]] static void e_file_missing() { throw std::runtime_error("Input file is missing."); }
+		bool m_save_histograms_as_images;
+		std::string m_histogram_save_directory; 
+		std::string m_histogram_image_format; 
 };
--- src/Histogram_merger.cxx
+++ src/Histogram_merger.cxx
@@ -4,7 +4,10 @@
 
 Histogram_merger_program::Histogram_merger_program(Histogram_merger_options&& t_options):
 	m_input_file_path_list(t_options.input_file_path_list),
-	m_output_file_path(t_options.output_file_path)
+	m_output_file_path(t_options.output_file_path),
+	m_save_histograms_as_images(t_options.save_histograms_as_images),
+	m_histogram_save_directory(t_options.histogram_save_directory),
+	m_histogram_image_format(t_options.histogram_image_format)
 {
 	std::for_each(m_input_file_path_list.begin(), m_input_file_path_list.end(), [&] (const std::string& input_file_name)
 	{
@@ -18,6 +21,7 @@ Histogram_merger_program::Histogram_merger_program(Histogram_merger_options&& t_
 	m_output_file = TFile::Open(m_output_file_path.c_str(), "RECREATE");
 	fetch_histograms_from_input_files();
 	m_output_file -> Write();
+	if(m_save_histograms_as_images) save_all_histograms_as_images();
 }
 
 // Generate a list of histograms in each of the nested directories in order to handle the data
@@ -28,17 +32,14 @@ void Histogram_merger_program::fetch_histograms_from_input_files()
 		file -> cd();
 		std::cout << "\n --- Listing keys in file: " << file -> GetName() << " ---\n";
 		this -> handle_data_object({}, file);
-		// for(auto&& key: *(file -> GetListOfKeys()))
-		// {
-		// 	handle_data_object(gDirectory -> Get(key -> GetName()));
-		// }
 	});
 }
 
 // Handle data object located in path: loop on it if directory, copy if histo
-void Histogram_merger_program::handle_data_object(const std::vector<std::string>& t_path, TObject* t_object)
+// This seemed easier to implement recursively than sequencially - could be subject to heavy optimizations (if anyone cared enough :))
+void Histogram_merger_program::handle_data_object(const std::vector<std::string>& t_path, const TObject* t_object)
 {
-	TDirectory* directory = dynamic_cast<TDirectory*>(t_object);
+	const TDirectory* directory = dynamic_cast<const TDirectory*>(t_object);
 	if(directory)
 	{
 		std::string directory_name = directory -> GetName();
@@ -47,37 +48,33 @@ void Histogram_merger_program::handle_data_object(const std::vector<std::string>
 		for(auto&& key: *(directory -> GetListOfKeys()))
 		{
 			std::cout << "Key name: " << key -> GetName() << std::endl;
-			handle_data_object(new_path, directory -> Get(key -> GetName()));
+			const TObject* new_object = const_cast<TDirectory*>(directory) -> Get(key -> GetName());
+			handle_data_object(new_path, new_object);
 		}
 		return;
 	}
-	TH1* histogram = dynamic_cast<TH1*>(t_object);
+	const TH1* histogram = dynamic_cast<const TH1*>(t_object);
+	if(histogram)
 	{
 		std::cout << "Handling histogram: " << t_object -> GetName() << '\n';
 		copy_histogram_to_output_file(t_path, histogram);
 	}
 }
 
-std::vector<std::string> Histogram_merger_program::append_to_path_copy(const std::vector<std::string>& t_path, const std::string& t_directory)
+void Histogram_merger_program::copy_histogram_to_output_file(const std::vector<std::string>& t_path, const TH1* t_histogram)
 {
-	std::vector<std::string> res(t_path);
-	res.push_back(t_directory);	
-	return res;
-}
-
-void Histogram_merger_program::copy_histogram_to_output_file(const std::vector<std::string>& t_path, TH1* t_histogram)
-{
+	if(t_path.size() == 0) return; // Should not happen, is at least expected to contain the file name
 	m_output_file -> cd();
-	std::for_each(t_path.begin(), t_path.end(), [&] (const auto& directory_name)
+	// Adding + 1 to begin to omit the filename
+	std::for_each(t_path.begin() + 1, t_path.end(), [&] (const auto& directory_name)
 	{
 		if(gDirectory -> GetDirectory(directory_name.c_str()) == nullptr)
 		{
-			std::cout << "Creating " << directory_name << std::endl;
+			std::cout << "Creating " << directory_name << '\n';
 			gDirectory -> mkdir(directory_name.c_str());
 		}
 		std::cout << "Entering " << directory_name << std::endl;
 		gDirectory -> cd(directory_name.c_str());
-		gDirectory -> pwd();
 	});
 	std::string histogram_name = t_histogram -> GetName();
 	TH1* histogram_in_output = dynamic_cast<TH1*>(gDirectory -> Get(histogram_name.c_str()));
@@ -92,4 +89,41 @@ void Histogram_merger_program::copy_histogram_to_output_file(const std::vector<s
 		// gDirectory -> WriteObject(t_histogram);
 		t_histogram -> Write();
 	}
+}
+
+std::vector<std::string> Histogram_merger_program::append_to_path_copy(const std::vector<std::string>& t_path, const std::string& t_directory)
+{
+	std::vector<std::string> res(t_path);
+	res.push_back(t_directory);	
+	return res;
+}
+
+void Histogram_merger_program::save_all_histograms_as_images()
+{
+	std::cout << "Saving histograms...\n";
+	std::stack<TObject*> objects_to_handle(std::deque<TObject*>{ m_output_file });
+	while(objects_to_handle.size())
+	{
+		const TObject* object = objects_to_handle.top();
+		objects_to_handle.pop();
+		const TDirectory* directory = dynamic_cast<const TDirectory*>(object);
+		if(directory)
+		{
+			std::cout << "Entering directory " << directory -> GetName() << '\n';
+			for(auto&& key: *(directory -> GetListOfKeys()))
+			{
+				objects_to_handle.emplace(const_cast<TDirectory*>(directory) -> Get(key -> GetName()));
+			}
+			continue;
+		}
+		const TH1* histogram = dynamic_cast<const TH1*>(object);
+		if(histogram)
+		{
+			TCanvas* canvas = new TCanvas(); // Not even bothering with trying to manage memory correctly
+			canvas -> cd();
+			const_cast<TH1*>(histogram) -> Draw();
+			canvas -> SaveAs((m_histogram_save_directory + '/' + histogram -> GetName() + '.' + m_histogram_image_format).c_str());
+		}
+	}
+	std::cout << "Done saving histograms.\n";
 }
